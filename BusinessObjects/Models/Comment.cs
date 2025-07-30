c31d0ad899cd2c6ce19df7534845352ac3dd54ea
HanTNnguyen2612 <trannguyenhan777@gmail.com>
2025-07-30 18:55:55 +0700

    done add business, fix bug

---
+++ BusinessObjects/Models/Comment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicWebMVC.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public virtual User User { get; set; }
+        public virtual Post? Post { get; set; }
+        public virtual ICollection<CommentReport> CommentReports { get; set; }
+    }
+}
